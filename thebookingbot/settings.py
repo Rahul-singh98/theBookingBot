ebab2262d96dafc1a7ec26571dbe199ed36c9c06
Rahul-singh98 <rajput.rahul8510@gmail.com>
2024-09-21 13:34:11 +0530

    Checked with nginx configurations and properly working

--- thebookingbot/settings.py
+++ thebookingbot/settings.py
@@ -22,11 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("CA_SECRET_KEY")
+penv = os.environ.get("ENVIROMENT", "development")
+
+IS_PRODUCTION = True if penv == "production" else False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not IS_PRODUCTION
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(',')
 
 
 # Application definition
@@ -75,15 +78,18 @@ TEMPLATES = [
 WSGI_APPLICATION = "thebookingbot.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    } if not IS_PRODUCTION else {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        # "PORT": os.getenv("DB_PORT", "5432"),
     }
-    
 }
 
 
@@ -134,3 +140,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Security settings for production
+SECURE_SSL_REDIRECT = IS_PRODUCTION # True
+CSRF_COOKIE_SECURE = IS_PRODUCTION # True
+SESSION_COOKIE_SECURE = IS_PRODUCTION # True
