3b6a730b8410d6c12943762629632f948f704eb7
rahul-singh98 <rajput.rahul8510@gmail.com>
2024-12-06 22:35:05 +0530

    Updated chatembed js

--- chatbotMicroservice/app/static/js/embed.js
+++ chatbotMicroservice/app/static/js/embed.js
@@ -565,7 +565,7 @@ const renderInput = (renderData) => {
     const autocompleteContainer = document.querySelector('.pac-container');
      // Style to adjust the suggestion list to appear above the input
     if (autocompleteContainer) {
-      const rect = inputElement.getBoundingClientRect();
+      const rect = clientBotState.elements.userInput.getBoundingClientRect();
       autocompleteContainer.style.position = 'absolute';
       autocompleteContainer.style.top = `${rect.top - autocompleteContainer.offsetHeight - 10}px`;
       autocompleteContainer.style.left = `${rect.left}px`;
